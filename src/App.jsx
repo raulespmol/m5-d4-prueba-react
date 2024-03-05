31ad46e8955dd02fdec560ace51736a32332fa4d
Raul Espinoza M <raul.espmol@gmail.com>
2024-03-05 18:00:13 -0300

    navbar base,

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import Navbar from './components/Navbar'
+import { Routes, Route } from 'react-router-dom'
 
 import './App.css'
+import Home from './views/Home'
+import Pizzas from './views/Pizzas'
+import PizzaDetail from './views/PizzaDetail'
+import Cart from './views/Cart'
+import NotFound from './views/NotFound'
 
 function App() {
 
   return (
     <>
       <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pizza" element={<Pizzas />} />
+        <Route path="/pizza/:name" element={<PizzaDetail />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,26 @@
+import { Container, Navbar as BNavbar, Nav } from "react-bootstrap"
+import { NavLink } from "react-router-dom"
+
 const Navbar = () => {
+  const setActiveClass = ({isActive}) => isActive ? 'active' : ''
   return (
-    <div>Navbar</div>
+    <BNavbar bg="dark">
+      <Container>
+        <div>
+          <NavLink to="/" className={setActiveClass}>
+            Home
+          </NavLink>
+          <NavLink to="/pizza" className={setActiveClass}>
+            Pizzas
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to="/cart" className={setActiveClass}>
+            Cart
+          </NavLink>
+        </div>
+      </Container>
+    </BNavbar>
   )
 }
 
