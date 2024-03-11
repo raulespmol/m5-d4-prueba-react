c0200f968b79053ecfb402cbbbd36c5ac112e84c
Raul Espinoza M <raul.espmol@gmail.com>
2024-03-11 19:21:08 -0300

    añadir al carrito desde Detail

--- src/views/PizzaDetail.jsx
+++ src/views/PizzaDetail.jsx
@@ -1,22 +1,44 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { AppContext } from '../context/AppContext';
-import PizzaCard from '../components/PizzaCard';
-import { Row, Col, Card } from 'react-bootstrap';
+import { Row, Col, Card, Button } from 'react-bootstrap';
 
 const PizzaDetail = () => {
   const { pizzaName } = useParams();
-  const { pizzas, formatName } = useContext(AppContext);
+  const { pizzas, cart, setCart, formatName } = useContext(AppContext);
+  const [count, setCount] = useState(1)
   const selectedPizza = pizzas.find(pizza => pizza.name === pizzaName);
 
+  const handleCount = (operator) => {
+    if(operator == 'add'){
+      setCount(count + 1)
+    }
+    if(operator == 'sub'){
+      count > 1 ? setCount(count - 1) : setCount(count)
+    }
+  }
+
+  const addCart = () => {
+    const pizzaIndex = cart.findIndex(item => item.id === selectedPizza.id)
+    if (pizzaIndex !== -1) {
+      const updatedCart = cart.map((item, index) =>
+        index === pizzaIndex ? { ...item, count: item.count + count  } : item
+      );
+      setCart(updatedCart);
+    } else {
+      const updatedPizza = { ...selectedPizza, count: count };
+      const updatedCart = [...cart, updatedPizza ];
+      setCart(updatedCart);
+    }
+  }
+
   if (!selectedPizza) {
-    return <div>No se encontró la pizza seleccionada</div>;
+    return <h3 className='text-center'>No se encontró la pizza seleccionada</h3>;
   }
 
   const {name, img, desc, ingredients, price } = selectedPizza
 
   return (
-    <>
       <Row className='pt-3'>
         <Col lg={6} xl={5}>
           <Card>
@@ -24,7 +46,7 @@ const PizzaDetail = () => {
           </Card>        
         </Col>
         <Col lg={6} xl={7}>
-          <h2 className='text-center mb-3'>{formatName(name)}</h2>
+          <h2 className='text-center mb-3'>{formatName(name)} - ${price}</h2>
           <hr />
           <p>{desc}</p>
           <ul>
@@ -36,14 +58,28 @@ const PizzaDetail = () => {
               )}
             )}
           </ul>
+          <div className='d-flex align-items-center justify-content-between w-100'>
+            <div>
+              <Button 
+              size='sm'
+              variant='secondary'
+              onClick={() => handleCount('sub')}>
+                - 
+              </Button>
+              <span className='px-3'>{count}</span>
+              <Button 
+              size='sm'
+              variant='secondary'
+              onClick={() => handleCount('add')}>
+                + 
+              </Button>
+              <span className='px-3 fs-4'>${count * price}</span>
+            </div>
+            <Button onClick={() => addCart()}>Añadir</Button>
+
+          </div>
         </Col>
       </Row>
-      {/* <Row>
-        <Col>
-          <PizzaCard pizza={selectedPizza} from={'detail'}/>
-        </Col>
-      </Row> */}
-    </>
   );
 };
 
